88dda85aa7f2fbcd909732e4b0cd4bdd41e052b8
Euler <euler@mind.com>
2015-09-09 22:03:02 -0300

    Inicio.

---
+++ CustomerFundao.Test/CustomerFundao.Test/CustomerController.feature
@@ -0,0 +1,8 @@
+﻿Feature: CustomerController
+	Obtem todos os clientes ativos 
+
+@Customer
+Scenario: Get all active customers
+	Given I have a Customer Controller With Index Action
+	When I Request All Customers
+	Then the I should receive a json object
---
+++ CustomerFundao.Test/CustomerFundao.Test/CustomerControllerSteps.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Web.Mvc;
+using TechTalk.SpecFlow;
+using static CustomerFundao.Test.Common.TestHelper;
+
+namespace CustomerFundao.Test
+{
+    [Binding]
+    public class CustomerControllerSteps
+    {
+        [Given(@"I have a Customer Controller With Index Action")]
+        public void GivenIHaveACustomerControllerWithIndexAction()
+        {
+            var controller = new CustomerFundao.Controllers.CustomerController();
+            Bag.CustomerController = controller;
+        }
+        
+        [When(@"I Request All Customers")]
+        public void WhenIRequestAllCustomers()
+        {
+            var controller = Bag.CustomerController as Controller;
+            var result = controller.Index();
+            Bag.Result = result;
+        }
+
+        [Then(@"the I should receive a json object")]
+        public void ThenTheIShouldReceiveAJsonObject()
+        {
+            var result = Bag.Result as JsonResult;
+
+            Assert.IsInstanceOf<JsonResult>(result);
+        }
+
+
+    }
+}
